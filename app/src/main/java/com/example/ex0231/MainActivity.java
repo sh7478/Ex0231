d25bced6f9401f346044263692fd24ce7f571f61
sh7478 <sh7478@bs.amalnet.k12.il>
2025-11-02 17:16:18 +0200

    finished ex1

--- app/src/main/java/com/example/ex0231/MainActivity.java
+++ app/src/main/java/com/example/ex0231/MainActivity.java
@@ -8,17 +8,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.ex0231.ex1.Chargeable;
+import com.example.ex0231.ex1.Laptop;
+import com.example.ex0231.ex1.Phone;
+import com.example.ex0231.ex1.Tablet;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Chargeable[] chargeables = new Chargeable[6];
+        chargeables[0] = (new Laptop());
+        chargeables[1] = (new Laptop());
+        chargeables[2] = (new Phone());
+        chargeables[3] = (new Phone());
+        chargeables[4] = (new Tablet());
+        chargeables[5] = (new Tablet());
+        for(Chargeable charges : chargeables)
+        {
+            charges.charge();
+        }
+
     }
 }
+
