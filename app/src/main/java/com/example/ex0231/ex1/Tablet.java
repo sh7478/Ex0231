57d4fb3b82e995bdd2d4664e1449b78b2944b43d
sh7478 <sh7478@bs.amalnet.k12.il>
2025-11-02 16:47:02 +0200

    added Tablet class

---
+++ app/src/main/java/com/example/ex0231/ex1/Tablet.java
@@ -0,0 +1,8 @@
+package com.example.ex0231.ex1;
+
+public class Tablet implements Chargeable{
+    @Override
+    public void charge() {
+        System.out.println("The tablet is charging");
+    }
+}
