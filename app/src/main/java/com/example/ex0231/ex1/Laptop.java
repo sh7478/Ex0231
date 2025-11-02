4b214d76389dede2bd259286b7b97a5509ce0386
sh7478 <sh7478@bs.amalnet.k12.il>
2025-11-02 16:48:28 +0200

    added Laptop class

---
+++ app/src/main/java/com/example/ex0231/ex1/Laptop.java
@@ -0,0 +1,8 @@
+package com.example.ex0231.ex1;
+
+public class Laptop implements Chargeable{
+    @Override
+    public void charge() {
+        System.out.println("The laptop is charging");
+    }
+}
