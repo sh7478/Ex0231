903e03affb6c84bf63ab573b45db325fd46cda98
sh7478 <sh7478@bs.amalnet.k12.il>
2025-11-07 17:24:45 +0200

    added playable and guitar

---
+++ app/src/main/java/com/example/ex0231/ex2/Guitar.java
@@ -0,0 +1,9 @@
+package com.example.ex0231.ex2;
+
+public class Guitar implements Playable{
+
+    @Override
+    public void play() {
+        System.out.println("the guitar has been played");
+    }
+}
