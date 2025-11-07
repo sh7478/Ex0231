944c8509e6a678c1c59afaf2470e346246bfd1ea
sh7478 <sh7478@bs.amalnet.k12.il>
2025-11-07 17:28:03 +0200

    added drum

---
+++ app/src/main/java/com/example/ex0231/ex2/Drum.java
@@ -0,0 +1,9 @@
+package com.example.ex0231.ex2;
+
+public class Drum implements Playable{
+
+    @Override
+    public void play() {
+        System.out.println("The drum has been played");
+    }
+}
