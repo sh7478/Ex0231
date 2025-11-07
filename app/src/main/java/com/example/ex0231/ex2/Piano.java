22065b0f1a7fdc37d0915d56316e48a359f82139
sh7478 <sh7478@bs.amalnet.k12.il>
2025-11-07 17:25:36 +0200

    added piano

---
+++ app/src/main/java/com/example/ex0231/ex2/Piano.java
@@ -0,0 +1,9 @@
+package com.example.ex0231.ex2;
+
+public class Piano implements Playable{
+
+    @Override
+    public void play() {
+        System.out.println("the piano has been played");
+    }
+}
